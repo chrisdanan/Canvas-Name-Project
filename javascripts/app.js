5cc09a5bf3b73e77490f76c71724cf1970008c3e
Christopher Danan <chrisddan@gmail.com>
2015-08-26 15:57:45 -0700

    Add two buttons, one which makes text have random colors and one which returns text to default state. -Center canvas area. -Add code for random colors.

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,12 +1,47 @@
-var canvas,
-	ctx;
+var canvas,		//Canvas object.
+	ctx,		//Context of canvas = 2d.
+	canvasWidth,
+	canvasHeight,
+	name = "Vanessa",
+	nameWidth = 500;
 
 var draw = function(){
 	"use strict";
-	
-	ctx.font = "5em helvetica";
 
-	ctx.fillText("Vanessa", 100, 200);
+	ctx.textAlign = "center";
+	ctx.font = "7em verdana";
+	ctx.fillStyle = "#000";
+
+	var nameLength = name.length,
+		x = (canvasWidth / 2) - (nameWidth / 2);
+
+	ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
+	for(var i = 0; i < nameLength; i++){
+		ctx.fillText(name.charAt(i), x, canvasHeight / 2);
+		x += ctx.measureText(name.charAt(i)).width;
+	}
+};
+
+var randColor = function(){
+	"use strict";
+
+	var nameLength = name.length,
+		x = (canvasWidth / 2) - (nameWidth / 2);
+
+	ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
+	for(var i = 0; i < nameLength; i++){
+		var r, g, b;
+
+		r = Math.floor(Math.random() * 256);
+		g = Math.floor(Math.random() * 256);
+		b = Math.floor(Math.random() * 256);
+
+		ctx.fillStyle = "rgb(" + r + ", " + g + ", " + b + ")";
+		ctx.fillText(name.charAt(i), x, canvasHeight / 2);
+		x += ctx.measureText(name.charAt(i)).width;
+	}
 };
 
 var main = function(){
@@ -15,11 +50,25 @@ var main = function(){
 	console.log("Hello Vane");
 
 	canvas = document.getElementById("canvas");
+	canvasWidth = canvas.width;
+	canvasHeight = canvas.height;
 
 	if(canvas.getContext){
 		ctx = canvas.getContext("2d");
 
 		draw();
+
+		$("#defaultBtn").on("click", function(){
+			console.log("Default button clicked.");
+
+			draw();
+		});
+
+		$("#randColorBtn").on("click", function(){
+			console.log("Random Color Button clicked.");
+
+			randColor();
+		});
 	}
 };
 
