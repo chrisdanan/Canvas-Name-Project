49431d02187c86acd5944c16c8514ae90cc199a1
Christopher Danan <chrisddan@gmail.com>
2015-08-26 20:33:28 -0700

    Add code for light switch effect.

--- javascripts/app.js
+++ javascripts/app.js
@@ -3,17 +3,22 @@ var canvas,		//Canvas object.
 	canvasWidth,
 	canvasHeight,
 	name = "Vanessa",
-	nameWidth = 500;
+	nameWidth = 500,
+	circles = false;
 
 var draw = function(){
 	"use strict";
 
-	ctx.textAlign = "center";
-	ctx.font = "7em verdana";
-	ctx.fillStyle = "#000";
-
 	var nameLength = name.length,
 		x = (canvasWidth / 2) - (nameWidth / 2);
+
+	if($("#canvas").hasClass("light")){
+		$("#canvas").toggleClass("light");
+	};
+
+	ctx.textAlign = "center";
+	ctx.font = "10em monospace";
+	ctx.fillStyle = "#000";
 
 	ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 
@@ -21,6 +26,10 @@ var draw = function(){
 		ctx.fillText(name.charAt(i), x, canvasHeight / 2);
 		x += ctx.measureText(name.charAt(i)).width;
 	}
+
+	ctx.save();
+
+	circles = false;
 };
 
 var randColor = function(){
@@ -29,7 +38,12 @@ var randColor = function(){
 	var nameLength = name.length,
 		x = (canvasWidth / 2) - (nameWidth / 2);
 
+	if($("#canvas").hasClass("light")){
+		$("#canvas").toggleClass("light");
+	};
+
 	ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
 
 	for(var i = 0; i < nameLength; i++){
 		var r, g, b;
@@ -40,7 +54,39 @@ var randColor = function(){
 
 		ctx.fillStyle = "rgb(" + r + ", " + g + ", " + b + ")";
 		ctx.fillText(name.charAt(i), x, canvasHeight / 2);
-		x += ctx.measureText(name.charAt(i)).width;
+		x += (ctx.measureText(name.charAt(i+1)).width);
+	}
+
+	circles = false;
+};
+
+var lightSwitch = function(s){
+	"use strict";
+
+	$("#canvas").toggleClass("light");
+
+	ctx.save();
+
+	ctx.globalCompositeOperation = "destination-over";
+
+	if(circles === false){
+		ctx.fillStyle = "#FFF";
+		ctx.globalAlpha = 0.2;  //Set alpha value for all shapes declared from this point onwards.
+
+		//Draw the translucent circles.
+		for(var i = 0; i < 6; i++){
+			ctx.save();
+			ctx.beginPath();
+			ctx.translate(canvasWidth / 2, canvasHeight / 2);
+			ctx.scale(2, 1);
+			ctx.arc(0, 0, 10 + 30 * i, 0, Math.PI * 2, true);
+			ctx.fill();
+			ctx.restore();
+		}
+
+		ctx.restore();
+
+		circles = true;
 	}
 };
 
@@ -68,6 +114,12 @@ var main = function(){
 			console.log("Random Color Button clicked.");
 
 			randColor();
+		});
+
+		$("#lightSwitch").on("click", function(){
+			console.log("Light Switch clicked.");
+
+			lightSwitch();
 		});
 	}
 };
