83c11a039b779b81f3b697473558e15ca67b6190
Christopher Danan <chrisddan@gmail.com>
2015-08-27 14:25:43 -0700

    Add a flashlight effect.

--- javascripts/app.js
+++ javascripts/app.js
@@ -3,7 +3,30 @@ var canvas,		//Canvas object.
 	canvasWidth,
 	canvasHeight,
 	name = "Vanessa",
-	nameWidth = 500;
+	nameWidth = 500,
+	lightOn = false;
+
+var light = {
+	x: (canvasWidth / 2),
+	y: (canvasHeight / 2),
+	draw: function(){
+		ctx.globalCompositeOperation = "destination-over";
+
+		ctx.fillStyle = "rgb(255, 255, 255)";
+		ctx.globalAlpha = 0.15;  //Set alpha value for all shapes declared from this point onwards.
+
+		//Draw the translucent circles.
+		for(var i = 0; i < 6; i++){
+			ctx.save();
+			ctx.beginPath();
+			ctx.arc(this.x, this.y, 10 + 30 * i, 0, Math.PI * 2, true);
+			ctx.fill();
+			ctx.restore();
+		}
+
+		ctx.globalAlpha = 1;
+	}
+};
 
 var draw = function(){
 	"use strict";
@@ -61,6 +84,10 @@ var lightSwitch = function(s){
 	var nameLength = name.length,
 		x = (canvasWidth / 2) - (nameWidth / 2);
 
+	if($("#canvas").hasClass("light")){
+		$("#canvas").toggleClass("light");
+	};
+
 	$("#canvas").toggleClass("light");
 
 	ctx.clearRect(0, 0, canvasWidth, canvasHeight);
@@ -104,6 +131,38 @@ var lightSwitch = function(s){
 	}
 };
 
+var flashlight = function(){
+	"use strict";
+
+	var nameLength = name.length,
+		x = (canvasWidth / 2) - (nameWidth / 2);
+
+	if($("#canvas").hasClass("light")){
+		$("#canvas").toggleClass("light");
+	};
+
+	$("#canvas").toggleClass("light");
+
+	ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
+	ctx.save();
+
+	if($("#canvas").hasClass("light")){
+		//Create the text.
+
+		ctx.fillStyle = "#000";
+
+		for(var i = 0; i < nameLength; i++){
+			ctx.fillText(name.charAt(i), x, canvasHeight / 2);
+			x += ctx.measureText(name.charAt(i)).width;
+		}
+
+		//Initialize light.
+
+		light.draw();
+	}
+};
+
 var main = function(){
 	"use strict";
 
@@ -121,20 +180,40 @@ var main = function(){
 		$("#defaultBtn").on("click", function(){
 			console.log("Default button clicked.");
 
+			lightOn = false;
 			draw();
 		});
 
 		$("#randColorBtn").on("click", function(){
 			console.log("Random Color Button clicked.");
 
+			lightOn = false;
 			randColor();
 		});
 
 		$("#lightSwitch").on("click", function(){
 			console.log("Light Switch clicked.");
 
+			lightOn = false;
 			lightSwitch();
 		});
+
+		$("#flashLight").on("click", function(){
+			console.log("Flash Light button clicked.");
+
+			lightOn = true;
+			flashlight();
+		});
+
+		canvas.addEventListener('mousemove', function(e){
+			if(lightOn){
+				ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+			    light.x = e.clientX;
+			    light.y = e.clientY;
+			    flashlight();
+			    light.draw();
+			}
+});
 	}
 };
 
